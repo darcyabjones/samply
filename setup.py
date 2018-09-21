7d283cdfa2da2f8a32aa8683349c0e7cdf056637
Darcy Jones <darcy.ab.jones@gmail.com>
2018-09-21 15:40:46 +0800

    added cli path

--- setup.py
+++ setup.py
@@ -111,10 +111,9 @@ setup(
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    #entry_points={
-    #    'console_scripts': [
-    #        'sample=sample:main',
-    #    ],
-    #},
-    #scripts=["bin/catastrophy"],
+    entry_points={
+        'console_scripts': [
+            'samply=samply:main',
+        ],
+    },
 )
